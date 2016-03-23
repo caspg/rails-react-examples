71b26bba100bcbbc3dc50b7ac5eae9e65e23aabf
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-23 16:47:29 +0100

    [redux-router] add routerMiddleware to store

--- redux-router/app/react/store/index.js
+++ redux-router/app/react/store/index.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { routerMiddleware } from 'react-router-redux'
+import { browserHistory } from 'react-router';
+
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
+  const middleware = routerMiddleware(browserHistory)
+
   return createStore(
     rootReducer,
-    initialState
+    initialState,
+    applyMiddleware(middleware)
   );
 }
