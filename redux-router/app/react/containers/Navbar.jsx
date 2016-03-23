529c0a02e35c71e09c94f231203844d78f086033
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-23 16:24:08 +0100

    [redux-router] create Navbar container

--- redux-router/app/react/containers/Navbar.jsx
+++ redux-router/app/react/containers/Navbar.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
+import NavbarLinks from '../components/NavbarLinks';
+
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-default" style={{ borderRadius: 0 }}>
-        <div className="containerFluid">
+        <div className="container-fluid">
           <div className="navbar-header">
             <a className="navbar-brand" href="#">RailsReduxRouter Example</a>
           </div>
+
+          <NavbarLinks />
         </div>
       </nav>
     );
--- redux-router/app/react/index.js
+++ redux-router/app/react/index.js
@@ -6,11 +6,11 @@ RWR.run();
 integrationsManager.register('redux-store', RWRRedux.storeIntegrationWrapper);
 integrationsManager.register('redux-container', RWRRedux.containerIntegrationWrapper);
 
-import Navbar from './components/Navbar';
-RWR.registerComponent('Navbar', Navbar);
-
 import CounterStore from './store';
 RWRRedux.registerStore('CounterStore', CounterStore);
+
+import Navbar from './containers/Navbar';
+RWRRedux.registerContainer('Navbar', Navbar);
 
 import CounterApp from './containers/Root';
 RWRRedux.registerContainer('CounterApp', CounterApp);
