7dd80003de6928ca84fc59e7b755a05b8e8a87ac
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-24 10:27:17 +0100

    [redux-router] add lorem text to ScorePage

--- redux-router/app/react/containers/ScorePage.jsx
+++ redux-router/app/react/containers/ScorePage.jsx
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 export default class ScorePage extends Component {
   render() {
     return (
-      <div>ScorePage</div>
+      <div>
+        <h2>Score Board</h2>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>consectetur adipisicing elit.</li>
+          <li>possimus pariatur sed sunt!</li>
+        </ul>
+      </div>
     );
   }
 }
