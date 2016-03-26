f984eb1b4a50e2ac6a63c486c36570657dc988d8
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-26 12:55:03 +0100

    [basic-redux] use redux_store and redux_container in rails view

--- basic-redux/app/react/containers/App.jsx
+++ basic-redux/app/react/containers/App.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+
+import Counter from '../components/Counter';
+import * as CounterActions from '../actions';
 
 export default class App extends Component {
   render() {
+    const { dispatch, counter } = this.props;
+    const boundActionCreators = bindActionCreators(CounterActions, dispatch);
+
     return (
-      <div style={{ border: '1px solid #ccc', borderRadius: 5, padding: 20, marginTop: 50  }}>
-        {this.props.children}
+      <div className="container">
+        <div className="col-sm-offset-2 col-sm-8">
+          <div style={{ border: '1px solid #ccc', borderRadius: 5, padding: 20, marginTop: 50  }}>
+            <Counter
+              counter={this.props.counter}
+              {...boundActionCreators}
+            />
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-function mapStateToProps(state, ownProps) {
-  return { currentPath: ownProps.location.pathname };
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  };
 }
 
 export default connect(mapStateToProps)(App);
